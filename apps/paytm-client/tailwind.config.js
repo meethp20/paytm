e9bd1b516e2fa05a723e1b43b01997296d8a9cbd
Meeth Patel <meeth2060@gmail.com>
2025-07-10 19:43:20 +0530

    .

---
+++ apps/paytm-client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",  
+    "../../packages/ui/src/**/*.{js,ts,jsx,tsx}"
+    ,
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
