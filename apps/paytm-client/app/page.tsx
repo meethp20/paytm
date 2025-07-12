27825a66e07a641daea9d0067cdb81160de6a0a7
Meeth Patel <meeth2060@gmail.com>
2025-07-12 18:11:11 +0530

    weird as fuck

--- apps/paytm-client/app/page.tsx
+++ apps/paytm-client/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home(){
 
   return(
     <div className=" w-full h-full flex justify-center items-center">
-      <div className="w-[100px] h-[100px] bg-white text-black">
+      <div className="w-[100px] h-[100px] bg-white text-black ">
       Hi 
       </div>
     </div>
